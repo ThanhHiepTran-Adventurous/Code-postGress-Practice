50abdd3ac37a3c76c89072d6fe0baefb41806beb
get-go-now <nguyenhiep0456@gmail.com>
2022-07-17 21:01:48 +0700

    update file

--- Test-connection-postSQL/src/select_equals_like/App.java
+++ Test-connection-postSQL/src/select_equals_like/App.java
@@ -1,63 +1,61 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package test.connection.postsql;
+package select_equals_like;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import test.connection.postsql.DatabaseConnection;
 
-public class Main {
+public class App {
 
+    private static Scanner in = new Scanner(System.in);
     private static List<Actor> listOfActors;
 
-    private static void getAllFullname() { 
+    public static void findActorBySelect(String value) {
         Connection conn = null;
         PreparedStatement stm = null;
         ResultSet rs = null;
+        int count = 0;
         try {
-            String sql = "SELECT * FROM actor";
+            //dùng de noi chuoi
+            StringBuilder sql = new StringBuilder("SELECT * FROM actor ");
+            sql.append("WHERE first_name = ?");
             conn = DatabaseConnection.getConnection();
-            stm = conn.prepareStatement(sql);
+            stm = conn.prepareStatement(sql.toString());
+            stm.setString(1, value);
             rs = stm.executeQuery();
             while (rs.next()) {
-                int id = rs.getInt("actor_id"); 
+                int id = rs.getInt("actor_id");
                 String firstName = rs.getString("first_name");
                 String lastName = rs.getString("last_name");
                 String lastUpdate = rs.getTimestamp("last_update").toString();
                 Actor actor = new Actor(id, firstName, lastName, lastUpdate);
-            //    String fullName = firstName + " " + lastName + " " + lastUpdate;
                 listOfActors.add(actor);
-             
+                count++;
             }
+            if(count == 0) {
+                System.out.println("Ko co ten ban can tim");
+            }
+
         } catch (Exception e) {
-            return;
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-                if (stm != null) {
-                    stm.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
+            System.out.println(e.getMessage());
         }
     }
 
     public static void main(String[] args) {
-        // TODO code application logic here
         listOfActors = new ArrayList<>();
-        getAllFullname();
+        System.out.print("Enter search value: ");
+        String value = in.nextLine();
+        findActorBySelect(value);
         for (Actor actor : listOfActors) {
-            System.out.println(actor);
+            System.out.println(actor.toString());
         }
     }
+
 }
